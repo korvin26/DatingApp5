a35fa07143646e1d5e1f18acbaa94bfdecbc7f3e
Mark Lipovetski <marik644@gmail.com>
2021-11-07 15:21:23 +0200

    End of section 10

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -21,6 +21,7 @@ namespace API.Helpers
                     opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
 
             CreateMap<Photo, PhotoDto>();
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }
